0a4da60355320329c48154b50b49eb8091e5be51
Marcos-Antonio-Rodrigues-dos-Santos <126624403+Marcos-Antonio-Rodrigues-dos-Santos@users.noreply.github.com>
2024-03-03 23:39:17 -0300

    atualizacao "Aula1-Exercicios"

--- aula1.js
+++ aula1.js
@@ -1,13 +1,10 @@
-var nome;
-var altura;
-var peso;
-var massa;
-var resp;
-//M = peso (quilos) ÷ altura²
+function imc(){
 
-
-function imc(peso, altura){
-    massa = peso / (altura**2)
+    let nome = prompt("Qual seu nome ?");
+    let altura = prompt("Qual é sua altura");
+    let peso = prompt("Qual é o seu peso?");
+    let resp;
+    let massa = peso / (altura**2);
 
     if (massa < 16){
         resp = "Baixo peso muito grave"
@@ -36,11 +33,134 @@ function imc(peso, altura){
         resp = "Obesidade grau II"
     }
 
-    else if(massa > 40){
+    else if(40 < massa){
         resp = "Obesidade grau III"
     }
 
-    return(resp)
+    return(alert(resp))
 }
 
-console.log(imc(105, 180))
+
+
+function idade(){
+
+    let idade = prompt("Qual a sua idade ?");
+    let resp;
+
+    if(0 <= idade < 15){
+        resp = "Criança"
+    }
+
+    else if(15 <= idade < 30){
+        resp = "Jovem"
+    }
+
+    else if(30 <= idade < 60){
+        resp = "Adulto"
+    }
+
+    else if(idade >= 60){
+        resp = "Idoso"
+    }
+
+    return(alert(resp))
+}
+
+
+function nota(){
+
+    let notaf;
+
+    let peso1 = 2;
+    let peso2 = 5;
+    let peso3 = 3;
+
+    let nota1 = prompt("Qual é sua primeira nota ?");
+    let nota2 = prompt("Qual é sua segunda nota ?");
+    let nota3 = prompt("Qual é sua terceira nota ?");
+
+    notaf = (((peso1 * nota1) + (peso2 * nota2) + (peso3 * nota3)) / 10);
+
+    if(notaf > 10){
+        alert(`A média do aluno ${notaf} e sua classificação é "A+"`)
+    }
+
+    else if(10 > notaf > 9){
+        alert(`A média do aluno ${notaf} e sua classificação é "A"`)
+    }
+
+    else if(9 > notaf > 8){
+        alert(`A média do aluno ${notaf} e sua classificação é "B"`)
+    }
+
+    else if(8 > notaf > 7){
+        alert(`A média do aluno ${notaf} e sua classificação é "C"`)
+    }
+
+    else if(7 > notaf > 6){
+        alert(`A média do aluno ${notaf} e sua classificação é "D"`)
+    }
+
+    else if(6 > notaf > 5){
+        alert(`A média do aluno ${notaf} e sua classificação é "E"`)
+    }
+
+    else if(notaf < 5){
+        alert(`A média do aluno ${notaf} e sua classificação é "F"`)
+    }
+
+}
+
+function calcEntrega(){
+
+    let pecas = prompt("Quantas peças serão transportadas?");
+    let regiao = prompt("Para que região sera feita o transporte?\n1-Sul, 2-Sudeste, 3-Centro-Oeste");
+    let distancia = prompt("Quantos quilometros são a viagem?");
+    let precop;
+    let preco;
+
+    let rast = prompt("Você gostaria de restrear sua carga?\nDigite 'S'-sim ou 'N'-não");
+    let taxar = 0;
+    
+    if((rast.toUpperCase()) = "S"){
+        let taxar = 200
+        alert("Sua entrega sairá R$ 200 mais cara")
+    }
+
+    if(regiao = 1){
+         if(pecas <= 1000){
+            preco = (pecas + (distancia * 10) + taxar)
+         }
+         else if(pecas > 1000){
+            precop = pecas + ((pecas - 1000) * 0.90)
+            preco = (precop + (distancia * 10) + taxar)
+         }
+
+    else if(regiao = 2){
+        if(pecas <= 1000){
+            precop = (pecas * 1.20)
+            preco = (precop + (distancia * 10) + taxar)
+         }
+         else if(pecas > 1000){
+            precop = (pecas * 1.20) + ((pecas - 1000) * 1.054)
+            preco = (precop + (distancia * 10) + taxar)
+         }
+    }
+    else if(regiao = 3){
+        if(pecas <= 1000){
+            precop = (pecas * 1.30)
+            preco = (precop + (distancia * 10) + taxar)
+         }
+         else if(pecas > 1000){
+            precop = (pecas * 1.30) + ((pecas - 1000) * 1.131)
+            preco = (precop + (distancia * 10) + taxar)
+         }
+    }
+
+    alert(`Taxa do rastreamento: R$${taxar}\n
+            Taxa do frete pelas pecas: R$${precop}\n
+            Taxa do frete por quilômetro: R$${(distancia * 10)}\n
+            Taxa total do frete: R$${preco}`)
+    }
+
+}
